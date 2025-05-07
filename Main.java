39f6706a5c5b0b446f6e00717e93b309a82375d4
Paul Coppolino <hta.infinite@gmail.com>
2025-05-07 11:25:32 -0400

    Finalized Main.java

--- Main.java
+++ Main.java
@@ -10,23 +10,24 @@ public class Main {
         Scanner input = new Scanner(System.in);
         while(shouldContinue == true){
             System.out.println("Options For File Modification Are As Follows:");
-            System.out.println("1 : Add Text To File\n2 : Read Text From File\n3 : Delete Text From File\n4 : Abort Program");
+            System.out.println("1 : Add Text To File\n2 : Read Text From File\n3 : Delete Text From File");
             System.out.print("Please choose an option : ");
             int choice;
             try {
-                
                 choice = input.nextInt();
+                System.out.println("---------------------------");
                 switch(choice) {
                     case 1 -> T.addText(filePath);
                     case 2 -> T.readFile(file);
                     case 3 -> T.deleteText(filePath);
                     default -> System.out.println("Nice try, bud.");
                 }
+                
             } 
             catch (Exception e) {
                 throw(e);
             }
-            
+            System.out.println("---------------------------");
             System.out.println("Process Completed. Continue?");
             System.out.println("1 : Yes || 0 : No");
             choice = input.nextInt();
